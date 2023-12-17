5aa0e6b2cc1c7124f20b4560e4f6f5b83dfa3b7d
T. Daniel Crawford <crawdad@vt.edu>
2023-12-17 16:19:48 -0500

    Added documentation on a few funcs.

--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ from itertools import permutations
 from opt_einsum import contract
 
 def perm_parity(a):
+    """
+    Compute the parity (+1/-1) of a given permutation of a list of integers 
+    from 0,...,N-1.
+
+    Parameters
+    ----------
+    a: list of integers 0,...,N-1
+
+    Returns
+    -------
+    integer +1/-1
+    """
     parity = 1
     for i in range(0,len(a)-1):
         if a[i] != i:
@@ -13,7 +25,21 @@ def perm_parity(a):
     return parity
 
 
-def match_phase(bra, bra_basis, ket, ket_basis):
+def mo_overlap(bra, bra_basis, ket, ket_basis):
+    """
+    Compute the MO overlap matrix between two (possibly different) basis sets
+
+    Parameters
+    ----------
+    bra: MO coefficient matrix for the bra state (NumPy array)
+    bra_basis: Psi4 BasisSet object for the bra state
+    ket: MO coefficient matrix for the ket state (NumPy array)
+    ket_basis: Psi4 BasisSet object for the ket state
+
+    Returns
+    -------
+    S: MO-basis overlap matrix (NumPy array)
+    """
     # Get AO-basis overlap integrals
     mints = psi4.core.MintsHelper(bra_basis)
     if bra_basis = ket_basis:
@@ -23,6 +49,26 @@ def match_phase(bra, bra_basis, ket, ket_basis):
 
     # Transform to MO basis
     S = bra.T @ S_ao @ ket
+
+    return S
+
+def match_phase(bra, bra_basis, ket, ket_basis):
+    """
+    Compute the phases of the MOs in a ket state and match them to those 
+    of a given bra state
+
+    Parameters
+    ----------
+    bra: MO coefficient matrix for the bra state (NumPy array)
+    bra_basis: Psi4 BasisSet object for the bra state
+    ket: MO coefficient matrix for the ket state (NumPy array)
+    ket_basis: Psi4 BasisSet object for the ket state
+
+    Returns
+    -------
+    new_ket: The phase-adjusted ket state (NumPy array)
+    """
+    S = mo_overlap(bra, bra_basis, ket, ket_basis)
 
     # Compute normalization constant and phase, and correct phase of ket
     new_ket = ket.copy()
@@ -34,22 +80,34 @@ def match_phase(bra, bra_basis, ket, ket_basis):
     return new_ket
 
 
+# Compute overlap between two determinants in (possibly) different bases
 def det_overlap(bra, bra_basis, ket, ket_basis):
-    # Get AO-basis overlap integrals
-    mints = psi4.core.MintsHelper(bra_basis)
-    if bra_basis = ket_basis:
-        S_ao = mints.ao_overlap().np
-    else:
-        S_ao = mints.ao_overlap(bra_basis, ket_basis).np
+    """
+    Compute the overlap between two Slater determinants (represented by their
+    MO coefficient matrices) of equal length in (possibly) different basis sets
 
-    # Transform to MO basis
-    S = bra.T @ S_ao @ ket
+    Parameters
+    ----------
+    bra: MO coefficient matrix for the bra state (NumPy array)
+    bra_basis: Psi4 BasisSet object for the bra state
+    ket: MO coefficient matrix for the ket state (NumPy array)
+    ket_basis: Psi4 BasisSet object for the ket state
 
-    # Compute overlap
+    Returns
+    -------
+    overlap: scalar
+    """
+    # Sanity check
+    if (bra.shape[0] != ket.shape[0]) or (bra.shape[1] != ket.shape[0]):
+        raise Exception("Bra and Ket States do not have the same dimensions: (%d,%d) vs. (%d,%d)." % (bra.shape[0], bra.shape[1], ket.shape[0], ket.shape[1]))
+
+    N = bra.shape[1] # Number of MOs in the determinants
+    S = mo_overlap(bra, bra_basis, ket, ket_basis)
+
     overlap = 0.0
-    for perm in permutations(range(ndocc)):
+    for perm in permutations(range(N)):
         product = 1.0
-        for p in range(ndocc):
+        for p in range(N):
             product *= S[p][perm[p]] 
         overlap += product*parity(perm)
 
