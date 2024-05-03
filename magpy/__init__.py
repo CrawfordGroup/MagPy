2961c875c878da5aab72e40143a29f761383ab2b
T. Daniel Crawford <crawdad@vt.edu>
2024-05-03 15:10:49 -0400

    Remove extraneous code from init.

--- magpy/__init__.py
+++ magpy/__init__.py
@@ -7,13 +7,10 @@ from .hamiltonian import Hamiltonian
 from .hfwfn import hfwfn
 from .ciwfn import ciwfn
 from .ciwfn_so import ciwfn_so
-from .aat_hf import AAT_HF
-from .aat_ci import AAT_CI
-from .aat_ci_so import AAT_CI_SO
 from .hessian import Hessian
 from .apt import APT
-from .normal import normal
 from .aat import AAT
+from .normal import normal
 
 
 #from ._version import __version__
