cec873e59c1a5cb211015b0b371a0512f13332d7
T. Daniel Crawford <crawdad@vt.edu>
2024-05-05 17:03:07 -0400

    NOW adding MP2.

---
+++ magpy/mpwfn_so.py
@@ -0,0 +1,135 @@
+if __name__ == "__main__":
+    raise Exception("This file cannot be invoked on its own.")
+
+import numpy as np
+import psi4
+from opt_einsum import contract
+import psi4
+from .utils import DIIS
+
+
+class mpwfn_so(object):
+
+    def __init__(self, hfwfn):
+
+        self.hfwfn = hfwfn
+
+        ## Transform Hamiltonian to MO basis
+
+        # AO-basis one-electron Hamiltonian
+        h = self.hfwfn.H.T + self.hfwfn.H.V
+
+        # If there are frozen core orbitals, build and add the frozen-core operator 
+        # (core contribution to Fock operator) to the one-electron Hamiltonian
+        self.efzc = 0
+        nfzc = hfwfn.H.basisset.n_frozen_core()
+        if nfzc > 0:
+            C = self.hfwfn.C[:,:nfzc] # only core MOs
+            Pc = contract('pi,qi->pq', C.conj(), C)
+            ERI = self.hfwfn.H.ERI
+            hc = h + 2.0 * contract('pqrs,pq->rs', ERI, Pc) - contract('pqrs,ps->qr', ERI, Pc)
+            self.efzc = contract('pq,pq->', (h+hc), Pc)
+            h = hc
+
+        # Select active MOs
+        C = self.hfwfn.C[:,nfzc:]
+
+        # AO->MO two-electron integral transformation
+        ERI = self.hfwfn.H.ERI
+        ERI = contract('pqrs,sl->pqrl', ERI, C[:,hfwfn.ndocc-nfzc:])
+        ERI = contract('pqrl,rk->pqkl', ERI, C.conj()[:,:hfwfn.ndocc-nfzc])
+        ERI = contract('pqkl,qj->pjkl', ERI, C[:,hfwfn.ndocc-nfzc:])
+        ERI = contract('pjkl,pi->ijkl', ERI, C.conj()[:,:hfwfn.ndocc-nfzc])
+        ERI_MO = ERI
+
+        ## Translate Hamiltonian to spin orbital basis
+        data = ERI.dtype
+
+        nt = self.nt = 2*(hfwfn.nbf - nfzc) # assumes number of MOs = number of AOs
+        no = self.no = 2*(hfwfn.ndocc - nfzc)
+        nv = self.nv = self.nt - self.no
+
+        # Set up orbital subspace slices
+        o = self.o = slice(0, no)
+        v = self.v = slice(no, nt)
+        a = self.a = slice(0, nt)
+
+        # Convert to Dirac ordering
+        ERI = np.zeros((no, no, nv, nv), dtype=data)
+        for p in range(no):
+            for q in range(no):
+                for r in range(nv):
+                    for s in range(nv):
+                        ERI[p,q,r,s] = ERI_MO[p//2,r//2,q//2,s//2] * (p%2 == r%2) * (q%2 == s%2)
+                        ERI[p,q,r,s] -= ERI_MO[p//2,s//2,q//2,r//2] * (p%2 == s%2) * (q%2 == r%2)
+        self.ERI = ERI
+
+        # Build orbital energy denominators
+        eps = hfwfn.eps[nfzc:hfwfn.ndocc]
+        eps_occ = np.zeros((no))
+        for i in range(no):
+            eps_occ[i] = eps[i//2]
+        eps = hfwfn.eps[hfwfn.ndocc:]
+        eps_vir = np.zeros((nv))
+        for a in range(nv):
+            eps_vir[a] = eps[a//2]
+        Dia = eps_occ.reshape(-1,1) - eps_vir # For later when I add singles
+        Dijab = eps_occ.reshape(-1,1,1,1) + eps_occ.reshape(-1,1,1) - eps_vir.reshape(-1,1) - eps_vir
+        self.Dijab = Dijab
+
+
+
+    def solve(self, **kwargs):
+
+        # Extract kwargs
+        print_level = kwargs.pop('print_level', 0)
+
+        valid_normalizations = ['FULL', 'INTERMEDIATE']
+        normalization = kwargs.pop('normalization', 'FULL').upper()
+        if normalization not in valid_normalizations:
+            raise Exception(f"{normalization:s} is not an allowed choice of normalization.")
+        self.normalization = normalization
+
+        if print_level > 0:
+            print("\nNMO = %d; NACT = %d; NO = %d; NV = %d" % (self.hfwfn.nbf, self.nt, self.no, self.nv))
+
+        o = self.o
+        v = self.v
+        ERI = self.ERI
+        Dijab = self.Dijab
+
+        E0 = self.hfwfn.escf + self.hfwfn.H.enuc
+        if print_level > 0:
+            print("HFWFN ESCF (electronic) = ", self.hfwfn.escf)
+            print("HFWFN ESCF (total) =      ", self.hfwfn.escf + self.hfwfn.enuc)
+
+        # first-order wfn amplitudes -- intermediate normalization
+        C0 = 1.0
+        C2 = ERI/Dijab
+
+        # MP2 energy
+        emp2 = self.compute_mp2_energy(o, v, ERI, C2)
+
+        if print_level > 0:
+            print("MP2 Correlation Energy = ", emp2)
+            print("MP2 Total Energy       = ", emp2 + E0)
+
+        # Re-normalize if necessary
+        if self.normalization == 'FULL':
+            C0, C2 = self.normalize(o, v, C2)
+            norm = np.sqrt(C0*C0 + (1/4) * contract('ijab,ijab', C2.conj(), C2))
+            if print_level > 0:
+                print(f"Normalization check = {norm:18.12f}")
+            self.C0 = C0
+            self.C2 = C2
+
+        return emp2, C0, C2
+
+    def compute_mp2_energy(self, o, v, ERI, C2):
+        emp2 = (1/4) * contract('ijab,ijab->', C2, ERI)
+        return emp2
+
+    def normalize(self, o, v, C2):
+        N = 1.0/np.sqrt(1.0 + (1/4) * contract('ijab,ijab->', C2.conj(), C2))
+        C0 = N; C2 = N * C2
+        return C0, C2
---
+++ magpy/tests/test_016_MP2.py
@@ -0,0 +1,132 @@
+import psi4
+import magpy
+import pytest
+from ..data.molecules import *
+import numpy as np
+import os
+
+np.set_printoptions(precision=10, linewidth=200, threshold=200, suppress=True)
+
+def test_MP2_SO_H2O_STO3G():
+    psi4.core.clean_options()
+    psi4.core.clean_options()
+    psi4.set_memory('2 GB')
+    psi4.set_output_file('output.dat', False)
+    psi4.set_options({'scf_type': 'pk',
+                      'e_convergence': 1e-12,
+                      'd_convergence': 1e-12,
+                      'r_convergence': 1e-12})
+
+    psi4.set_options({'basis': 'STO-3G'})
+    mol = psi4.geometry(moldict["H2O"])
+    rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
+    print(f"  SCF Energy from Psi4: {rhf_e}")
+
+    psi4.set_options({'freeze_core': 'false'})
+    H = magpy.Hamiltonian(mol)
+    scf = magpy.hfwfn(H, 0, 1)
+    e_conv = 1e-13
+    r_conv = 1e-13
+    escf, C = scf.solve(e_conv=e_conv, r_conv=r_conv, print_level=1)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='intermediate', print_level=1)
+    c4mp2 = -0.049149636120
+    C0_ref = 1.0
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='full', print_level=1)
+    c4mp2 = -0.049149636120
+    C0_ref = 0.9891827770673378
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+    # Test frozen core
+    psi4.set_options({'freeze_core': 'true'})
+    H = magpy.Hamiltonian(mol)
+    scf = magpy.hfwfn(H, 0, 1)
+    e_conv = 1e-13
+    r_conv = 1e-13
+    escf, C = scf.solve(e_conv=e_conv, r_conv=r_conv, print_level=1)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='intermediate', print_level=1)
+    c4mp2 = -0.049060280876
+    C0_ref = 1.0
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='full', print_level=1)
+    c4mp2 = -0.049060280876
+    C0_ref = 0.9891844557765184
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+
+def test_MP2_SO_H2O_CCPVDZ():
+    psi4.core.clean_options()
+    psi4.set_memory('2 GB')
+    psi4.set_output_file('output.dat', False)
+    psi4.set_options({'scf_type': 'pk',
+                      'e_convergence': 1e-12,
+                      'd_convergence': 1e-12,
+                      'r_convergence': 1e-12})
+
+    psi4.set_options({'basis': 'cc-pVDZ'})
+    mol = psi4.geometry(moldict["H2O"])
+    rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
+    print(f"  SCF Energy from Psi4: {rhf_e}")
+
+    psi4.set_options({'freeze_core': 'false'})
+    H = magpy.Hamiltonian(mol)
+    scf = magpy.hfwfn(H, 0, 1)
+    e_conv = 1e-13
+    r_conv = 1e-13
+    escf, C = scf.solve(e_conv=e_conv, r_conv=r_conv, print_level=1)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='intermediate', print_level=1)
+    c4mp2 = -0.214347601415
+    C0_ref = 1.0
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='full', print_level=1)
+    c4mp2 = -0.214347601415
+    C0_ref = 0.9716969060925619
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+    psi4.set_options({'freeze_core': 'true'})
+    H = magpy.Hamiltonian(mol)
+    scf = magpy.hfwfn(H, 0, 1)
+    e_conv = 1e-13
+    r_conv = 1e-13
+    escf, C = scf.solve(e_conv=e_conv, r_conv=r_conv, print_level=1)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='intermediate', print_level=1)
+    c4mp2 = -0.212229959873
+    C0_ref = 1.0
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
+    mp2 = magpy.mpwfn(scf)
+    emp2, C0, C2 = mp2.solve(normalization='full', print_level=1)
+    c4mp2 = -0.212229959873
+    C0_ref = 0.9717330625421687
+    assert(abs(escf - rhf_e) < 1e-11)
+    assert(abs(emp2 - c4mp2) < 1e-11)
+    assert(abs(C0 - C0_ref) < 1e-11)
+
